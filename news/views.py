54816c11846acd86c95c8e348302415a74c34c4c
Andre8711 <1andrey171@mail.ru>
2025-08-19 22:56:52 +0300

    Обновил стили и шаблоны

--- news/views.py
+++ news/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
+from .models import News  # импортируем модель News
 
 def index(request):
-    # Главная страница
     return render(request, 'news/main.html')
 
 def news_list(request):
-    # Список новостей
-    news = []  # Здесь можно получить новости из базы
+    news = News.objects.all().order_by('-published_date')  # получаем новости из базы
     return render(request, 'news/news_list.html', {'news': news})
 
+# остальные функции без изменений
 def articles(request):
-    # Страница статей
     return render(request, 'news/articles.html')
 
 def contacts(request):
-    # Страница контактов
     return render(request, 'news/contacts.html')
 
 def category(request, category_name):
-    # Страница категории
     return render(request, 'news/category.html', {'category_name': category_name})
 
 def article_detail(request, article_slug):
-    # Страница статьи
     return render(request, 'news/article_detail.html', {'article_slug': article_slug})
+def news_list(request):
+    news = News.objects.all().order_by('-created_at')  # сортируем по дате создания
+    return render(request, 'news/news_list.html', {'news': news})
